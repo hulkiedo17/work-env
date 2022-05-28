1ec09e6f90d9d24f50dff273c87a39df7aaff33a
hulkiedo17 <hulkiedo@gmail.com>
2022-05-28 16:34:54 +0300

    added build script for test projects

---
+++ build.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+
+EXEC="$1"
+options=(-v -s -a -d -c -r)
+
+# -v
+valgrind_check() {
+	echo "build.sh: valgrind"
+	valgrind "-s" "--leak-check=full" "--track-origins=yes" "--show-leak-kinds=all" "./$EXEC"
+}
+
+# -s
+strace_check() {
+	echo "build.sh: strace"
+}
+
+# -a
+cppcheck_start() {
+	echo "build.sh: cppcheck"
+}
+
+# -d
+debug() {
+	echo "build.sh: debug"
+	gdb "./$EXEC"
+}
+
+# -c
+compile() {
+	echo "build.sh: compile"
+	gcc "./$EXEC.c" "-o" "./$EXEC" "-g" "-O0" "-Wall" "-Wextra" "-Werror" "-lconf"
+}
+
+# -r
+run_exec() {
+	echo "build.sh: run_exec"
+	./$EXEC
+}
+
+handle_options() {
+	for i in $@; do
+		case "$i" in
+			"${options[0]}")
+				valgrind_check
+				;;
+			"${options[1]}")
+				#
+				;;
+			"${options[2]}")
+				#
+				;;
+			"${options[3]}")
+				debug
+				;;
+			"${options[4]}")
+				compile
+				;;
+			"${options[5]}")
+				run_exec
+				;;
+			*)
+				echo "build.sh: unknown command"
+				;;
+		esac
+	done
+}
+
+handle_options "$@"
+exit 0
